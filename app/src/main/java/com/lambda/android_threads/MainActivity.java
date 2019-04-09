fd33a8834a755bab0c8c394941aec2950a6542be
Shoonsk8 <shoon@att.net>
2019-04-09 11:17:35 -0400

    progress bar

--- app/src/main/java/com/lambda/android_threads/MainActivity.java
+++ app/src/main/java/com/lambda/android_threads/MainActivity.java
@@ -1,19 +1,29 @@
 package com.lambda.android_threads;
 
+import android.app.ProgressDialog;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
+
 import android.view.View;
-import android.view.WindowInsets;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
     EditText et;
     TextView tv;
     String str;
     Button bt;
+    ProgressDialog progressBar;
+    private int progressBarStatus = 0;
+    private Handler progressBarHandler = new Handler();
+    private int iProgress = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,64 +38,105 @@ public class MainActivity extends AppCompatActivity {
         bt.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String debug="";
-                String s =  et.getText().toString();
-                if (s.equals( "" ))return;
+                progressBar = new ProgressDialog(v.getContext());
+                progressBar.setCancelable(true);
+                progressBar.setMessage("Processing ...");
+                progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressBar.setProgress(0);
+                progressBar.setMax(100);
+                progressBar.show();
+                //reset progress bar and filesize status
+                progressBarStatus = 0;
+                iProgress = 0;
+                new Thread(new Runnable() {
+                    public void run() {
+                        while (progressBarStatus < 100) {
+                            // performing operation
+                            progressBarStatus = doOperation();
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            // Updating the progress bar
 
-                int shift=Integer.parseInt( s );
-                s="";
-               int len = str.length();
-               // shift=8;
-                char c;
-                for(int x = 0; x < len; x++){
-                    c=str.charAt(x);
-                    if(c>=65&&c<=90){
-                        c=(char)(c+shift);
-                        debug+=(char)(c-shift); //debug
-                        if (c > 'Z') {
-                            s += (char) (c-26);
-                        }else{
-                            s +=(char)c;
+                            progressBarHandler.post(  new Runnable() {
+                                public void run() {
+                                    progressBar.setProgress(progressBarStatus);
+                                }
+                            });
                         }
-                    }else if(c>=97&&c<=122){
-                        c=(char)(c+shift);
-                        debug+=(char)(c-shift); //debug
-                        if (c > 'z') {
-                            s += (char) (c-26);
-                        }else {
-                            s += (char) c;
+                        // performing operation if file is downloaded,
+                        if (progressBarStatus >= 100) {
+                            // sleeping for 1 second after operation completed
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            // close the progress bar dialog
+                            progressBar.dismiss();
                         }
-                    }else{
-
-                        s+=(char)c;
                     }
-                }
-                tv.setText( s );
-                tv.invalidate();
+                }).start();
+
+
             }
         } );
-  /*      et.setOnKeyListener( new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if(keyCode==34) {
 
-                    tv.setText( et.getText() );
+
+
+
+    }
+    public int doOperation() {
+        //The range of ProgressDialog starts from 0 to 10000
+
+            String debug="";
+            String sTemp =  et.getText().toString();
+            if (sTemp.equals( "" ))return 100;
+
+            int shift=Integer.parseInt( sTemp );
+            sTemp="";
+            int len = str.length();
+            // shift=8;
+            char cTemp;
+            int iPitch=len/4; //frequency to return value
+            for(int x = 0; x < len; x++){
+
+                cTemp=str.charAt(x);
+                if(cTemp>=65&&cTemp<=90){
+                    cTemp=(char)(cTemp+shift);
+                    debug+=(char)(cTemp-shift); //debug
+                    if (cTemp > 'Z') {
+                        sTemp += (char) (cTemp-26);
+                    }else{
+                        sTemp +=(char)cTemp;
+                    }
+                }else if(cTemp>=97&&cTemp<=122){
+                    cTemp=(char)(cTemp+shift);
+                    debug+=(char)(cTemp-shift); //debug
+                    if (cTemp > 'z') {
+                        sTemp += (char) (cTemp-26);
+                    }else {
+                        sTemp += (char) cTemp;
+                    }
+                }else{
+
+                    sTemp+=(char)cTemp;
+                }
+                iProgress++;
+
+                if(iProgress%iPitch==0) {
+                    return 100*iProgress/len;
                 }
 
-                return true;
             }
-        } );*/
-/*        et.setOnApplyWindowInsetsListener( new View.OnApplyWindowInsetsListener() {
-                                               @Override
-                                               public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
-                                                   tv.setText( et.getText() );
-                                                   return null;
-                                               }
-                                           }
-        );
-*/
-    }
+            tv.setText( sTemp );
 
+
+
+        return 100;
+    }//end of doOperation
 
 }
 
